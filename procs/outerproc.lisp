cffe09996e9fe3f7ef55de40264b4fc61da55709
OMURA Shinichi <song.of.sand@gmail.com>
2023-06-21 10:36:35 +0900

    change manipulate log file
    create and append
    
    	modified:   20230621-processcomm.txt
    	modified:   outerproc.lisp

--- procs/outerproc.lisp
+++ procs/outerproc.lisp
@@ -3,13 +3,29 @@
 (load "procs-package.lisp")
 (in-package :procloop)
 
+(defun create-olog (logfile)
+  (unless (probe-file logfile)
+     (with-open-file (out logfile
+                   :direction :output
+                   :if-exists :supersede)
+     )
+  )
+)
+
 (defun outsider (gi ofile)
-  (prog (cmd r)
+  (prog (cmd r (logf #p"olog3"))
+    (create-olog logf)
     (setq cmd (with-output-to-string (out)
       (format out "sbcl --script innerproc.lisp  ~a '~a'" gi ofile)))
     (format t "cmd = ~a~%" cmd)
-    (setq r (uiop:run-program cmd  :output "olog2" :IF-OUTPUT-EXISTS :supersede :force-output t))
+    (setq r (uiop:run-program cmd :output logf :IF-OUTPUT-EXISTS :append :force-output t))
     (return r)
   )
 )
+
+;;; --scriptはreplしない。--loadは普通の動きになるのでpromptがでたりする
+;; logとるつもりなら--loadがよいかも。
+;;実行するだけなら--scriptがよいようだ
+; (format out "sbcl --script innerproc.lisp  ~a '~a'" gi ofile)))
+; (format out "sbcl --load innerproc.lisp  ~a '~a'" gi ofile)))
 
